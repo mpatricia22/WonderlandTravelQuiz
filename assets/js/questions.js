ef3da5126b40898489a76315f312a65147d408c7
mpatricia22 <marciapat22@gmail.com>
2023-02-28 19:44:39 +0000

    add questions

---
+++ assets/js/questions.js
@@ -0,0 +1,70 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "What is the most Northern Capital City??",
+  answer: "Reykjavik",
+  options: [
+    "Reykjavik",
+    "Helsinki",
+    "Oslo",
+    "Hyper Tool Multi Language"
+  ]
+},
+  {
+  numb: 2,
+  question: "How many Emirates does the United Arab Emirates (UAE) have?",
+  answer: "7",
+  options: [
+    "7",
+    "6",
+    "2",
+    "1"
+  ]
+},
+  {
+  numb: 3,
+  question: " Zanzibar is an island off which African Country?",
+  answer: "Tanzania",
+  options: [
+    "Nigeria",
+    "Senegal",
+    "Tanzania",
+    "Kenya"
+  ]
+},
+  {
+  numb: 4,
+  question: "The Film ‘The Beach’ was filmed in which Country?",
+  answer: "Thailand",
+  options: [
+    "Thailand",
+    "Laos",
+    "Indonesia",
+    "Thailand"
+  ]
+},
+  {
+  numb: 5,
+  question: " What is the Capital City of Turkey?",
+  answer: "Anakara",
+  options: [
+    "Antalya",
+    "Istanbul",
+    "Izmir",
+    "Ankara"
+  ]
+},
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
