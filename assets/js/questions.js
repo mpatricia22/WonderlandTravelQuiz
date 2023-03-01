df6c407ca147be32eb916db2f965efa57222ede9
mpatricia22 <marciapat22@gmail.com>
2023-03-01 10:41:05 +0000

    home page updates

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,70 +1,95 @@
 // creating an array and passing the number, questions, options, and answers
-let questions = [
+let questions = [{
+    numb: 1,
+    question: "What is the most Northern Capital City??",
+    answer: "Reykjavik",
+    options: [
+      "Reykjavik",
+      "Helsinki",
+      "Oslo",
+      "Hyper Tool Multi Language"
+    ]
+  },
   {
-  numb: 1,
-  question: "What is the most Northern Capital City??",
-  answer: "Reykjavik",
-  options: [
-    "Reykjavik",
-    "Helsinki",
-    "Oslo",
-    "Hyper Tool Multi Language"
-  ]
-},
+    numb: 2,
+    question: "How many Emirates does the United Arab Emirates (UAE) have?",
+    answer: "7",
+    options: [
+      "7",
+      "6",
+      "2",
+      "1"
+    ]
+  },
   {
-  numb: 2,
-  question: "How many Emirates does the United Arab Emirates (UAE) have?",
-  answer: "7",
-  options: [
-    "7",
-    "6",
-    "2",
-    "1"
-  ]
-},
+    numb: 3,
+    question: " Zanzibar is an island off which African Country?",
+    answer: "Tanzania",
+    options: [
+      "Nigeria",
+      "Senegal",
+      "Tanzania",
+      "Kenya"
+    ]
+  },
   {
-  numb: 3,
-  question: " Zanzibar is an island off which African Country?",
-  answer: "Tanzania",
-  options: [
-    "Nigeria",
-    "Senegal",
-    "Tanzania",
-    "Kenya"
-  ]
-},
+    numb: 4,
+    question: "The Film ‘The Beach’ was filmed in which Country?",
+    answer: "Thailand",
+    options: [
+      "Thailand",
+      "Laos",
+      "Indonesia",
+      "Thailand"
+    ]
+  },
   {
-  numb: 4,
-  question: "The Film ‘The Beach’ was filmed in which Country?",
-  answer: "Thailand",
-  options: [
-    "Thailand",
-    "Laos",
-    "Indonesia",
-    "Thailand"
-  ]
-},
-  {
-  numb: 5,
-  question: " What is the Capital City of Turkey?",
-  answer: "Anakara",
-  options: [
-    "Antalya",
-    "Istanbul",
-    "Izmir",
-    "Ankara"
-  ]
-},
+    numb: 5,
+    question: " What is the Capital City of Turkey?",
+    answer: "Anakara",
+    options: [
+      "Antalya",
+      "Istanbul",
+      "Izmir",
+      "Ankara"
+    ]
+  },
 
-//   {
-//   numb: 6,
-//   question: "Your Question is Here",
-//   answer: "Correct answer of the question is here",
-//   options: [
-//     "Option 1",
-//     "option 2",
-//     "option 3",
-//     "option 4"
-//   ]
-// },
+  {
+    numb: 6,
+    question: "Which one of these Australian animals is known for its smile?",
+    answer: "Quokka",
+    options: [
+      "Tasmanian Devil",
+      "Koala",
+      "Kangaroo",
+      "Quokka"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the capital city of Afghanistan?",
+    answer: "Kabul",
+    options: [
+      "Herat",
+      "Kabul",
+      "Tehran",
+      "Islamabad"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In which capital city can you find the Petronas Twin Towers?",
+    answer: "Kuala Lumpur",
+    options: [
+      "Buenos Aires",
+      "Bangkok",
+      "Dubai",
+      "Kuala Lumpur"
+    ]
+  },
+
+  
+
+
 ];
